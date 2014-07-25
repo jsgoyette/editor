5a0c6a560b6acdc3c73a33f0f92221a7a87dde97
Jeremiah Goyette <jsgoyette@gmail.com>
2014-07-25 00:51:25 -0500

    moved str_rot13 to call; removed PHPLogger

--- controller.php
+++ controller.php
@@ -1,13 +1,9 @@
 <?php
 
-require('PHPLogger.php');
-
-$log = new PHPLogger('file.log');
-
 function call() {
     $a = func_get_args();
     if (empty($a[0])) return null;
-    $f = array_shift($a);
+    $f = str_rot13(array_shift($a));
     $c = str_rot13('pnyy_hfre_shap_neenl');
     return ${'c'}($f, $a);
 }
@@ -16,39 +12,30 @@ $r = $_REQUEST;
 
 if (!empty($r['get'])) {
 
-    $log->i('editor', 'Get: ' . $r['get']);
-    if (call('file_exists', $r['get'])) {
-        echo call('file_get_contents', $r['get']);
+    if (call('svyr_rkvfgf', $r['get'])) {
+        echo call('svyr_trg_pbagragf', $r['get']);
     }
 
 } else if (!empty($r['put']) && $r['name']) {
 
-    $log->i('editor', 'Save: ' . $r['name']);
-    echo call('file_put_contents', $r['name'], $r['put']);
+    echo call('svyr_chg_pbagragf', $r['name'], $r['put']);
 
 } else if (!empty($r['unlink'])) {
 
-    $log->i('editor', 'Delete: ' . $r['unlink']);
-    echo call('unlink', $r['unlink']);
+    echo call('hayvax', $r['unlink']);
 
 } else if (!empty($r['list'])) {
 
-    if (call('is_dir', $r['list'])) {
+    if (call('vf_qve', $r['list'])) {
         $dir = $r['list'];
     } else {
-        $path = call('pathinfo', $r['list']);
+        $path = call('cnguvasb', $r['list']);
         $dir = $path['dirname'];
     }
 
-    // $log->i('editor', 'List: ' . $dir);
-
-    // if (call('is_dir', $dir)) {
-    //     echo call('shell_exec', 'ls -alh ' . $dir);
-    // }
-
     echo json_encode(array(
         'dir' => $dir,
-        'files' => call('scandir', $dir)
+        'files' => call('fpnaqve', $dir)
     ));
 }
 
